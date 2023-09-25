72002e5a4f9c86f639d57d769bea7dd68cf5dfcd
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 23:48:24 +0400

    Change example status to out of maintenance

--- CS/XtraReport1.cs
+++
@@ -1,9 +0,0 @@
-
-namespace CreateSubreport {
-    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
-        public XtraReport1() {
-            InitializeComponent();
-        }
-
-    }
-}
